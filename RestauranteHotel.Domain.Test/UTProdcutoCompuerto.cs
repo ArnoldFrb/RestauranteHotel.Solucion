5481145f6587f6dcfef67dbefccc1c3b5b1c7202
SLaxum <55930519+SLaxum@users.noreply.github.com>
2021-03-30 19:27:30 -0500

    CASOS DE PRUEBAS

--- RestauranteHotel.Domain.Test/UTProdcutoCompuerto.cs
+++ RestauranteHotel.Domain.Test/UTProdcutoCompuerto.cs
@@ -45,12 +45,12 @@ namespace RestauranteHotel.Domain.Test
          Existencias negativas o de cero 
             H1: Como Usuario quiero solicitar las existencia del producto para mantener la informacion actualizada.
             Criterio de Aceptación:
-            1.2 la existencia no puede ser menor o igual a 0.
+            1.2 la existencia puede ser  mayor a 0 Y mayor a Existencia de los producto.
             Dado Exite producto en el inventario 
             id "111", Nombre "Queso", Existencia 50, Precio 700, Costo 500
             id "112", Nombre "Pan", Existencia 30, Precio 700, Costo 600
             id "113", Nombre "Salchicha", Existencia 40, Precio 800, Costo 500
-            Cuando Ingrese una Existencia Menor o igual a 0; => Existencia = 0
+            Cuando Ingrese una Existencia mayor a 0 Y mayor a Existencia de los producto; => Existencia = 35
             Entonces El sistema presentará el mensaje. “Registro Fallido, La Existencia solicitada 35 del prodcuto Pan es Mayor a 30”
         */
         [Test]
@@ -74,13 +74,13 @@ namespace RestauranteHotel.Domain.Test
          Existencias negativas o de cero 
             H1: Como Usuario quiero solicitar las existencia del producto para mantener la informacion actualizada.
             Criterio de Aceptación:
-            1.2 la existencia no puede ser menor o igual a 0.
+            1.2 la existencia puede ser mayor a 0 Y menor o igual a Existencia de los producto.
             Dado Exite producto en el inventario 
             id "111", Nombre "Queso", Existencia 50, Precio 700, Costo 500
             id "112", Nombre "Pan", Existencia 30, Precio 700, Costo 600
             id "113", Nombre "Salchicha", Existencia 40, Precio 800, Costo 500
-            Cuando Ingrese una Existencia Menor o igual a 0; => Existencia = 0
-            Entonces El sistema presentará el mensaje. “Registro Fallido, La Existencia solicitada 35 del prodcuto Pan es Mayor a 30”
+            Cuando Ingrese una Existencia mayor a 0 Y menor o igual a Existencia de los producto; => Existencia = 2
+            Entonces El sistema presentará el mensaje. “Venta Exitosa, Cantidad: 2; Costo: 1.600,00; Precio: 2.500,00; Utilidad: 1.800,00”
         */
         [Test]
         public void PuedeSoloicitarExistenciaMayorALaExistenciaDelProductoYMayorACero()
