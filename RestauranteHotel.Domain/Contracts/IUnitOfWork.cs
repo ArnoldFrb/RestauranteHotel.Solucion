0a341126486bab574bdcb2d59560918f632daf5c
Escalab Mario <55930519+ArnoldFrb@users.noreply.github.com>
2021-05-03 10:28:28 -0500

    Infrastructure.Data

--- RestauranteHotel.Domain/Contracts/IUnitOfWork.cs
+++ RestauranteHotel.Domain/Contracts/IUnitOfWork.cs
@@ -4,9 +4,6 @@ namespace RestauranteHotel.Domain.Contracts
 {
     public interface IUnitOfWork
     {
-        IProductoRepository ProductoRepository { get; }
-        IProductoCompuestoRepository ProductoCompuesto { get; }
-        ProductoSiempleRepository ProductoSimple { get; }
         void Commit();
     }
 }
