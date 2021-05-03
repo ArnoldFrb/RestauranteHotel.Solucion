39cde78bdd0370962774a96e158d7f16c6cb9984
Escalab Mario <55930519+ArnoldFrb@users.noreply.github.com>
2021-05-03 13:57:10 -0500

    Migrations

--- RestauranteHotel.Infrastructure.Data/RestauranteHotelContext.cs
+++ RestauranteHotel.Infrastructure.Data/RestauranteHotelContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-<<<<<<< Updated upstream
 using RestauranteHotel.Infrastructure.Data.Base;
 using RestauranteHotel.Domain.Entity;
-=======
-using RestauranteHotel.Domain.Entity;
-using RestauranteHotel.Infrastructure.Data.Base;
->>>>>>> Stashed changes
 using System;
 
 namespace RestauranteHotel.Infrastructure.Data
@@ -18,22 +13,23 @@ namespace RestauranteHotel.Infrastructure.Data
         }
 
         public DbSet<Producto> Productos { get; set; }//equivale a Repositorios
-<<<<<<< Updated upstream
         public DbSet<ProductoSimple> ProductoSimples { get; set; }//equivale a Repositorios
         public DbSet<ProductoCompuesto> ProductoCompuestos { get; set; }//equivale a Repositorios
-=======
->>>>>>> Stashed changes
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Producto>().HasKey(c => c.Id);
+<<<<<<< Updated upstream
            
 
             modelBuilder.Entity<Producto>().ToTable("Producto");
             modelBuilder.Entity<ProductoSimple>().ToTable("ProductoSimple");
             modelBuilder.Entity<ProductoCompuesto>().ToTable("ProductoCompuesto");
+=======
+>>>>>>> Stashed changes
 
-            //inicailizacion de datos 
+            modelBuilder.Entity<ProductoSimple>().ToTable("ProductoSimple");
+            modelBuilder.Entity<ProductoCompuesto>().ToTable("ProductoCompuesto");
             //modelBuilder.Entity<CuentaBancaria>().HasData(new  { Id=1, Numero="1010", Ciudad="Valleduar", Email="Email"} );
             //modelBuilder.Entity<CuentaBancaria>().HasData(new { Id = 1, Numero = "1010", Ciudad = "Valleduar", Email = "Email" });
         }
